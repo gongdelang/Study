21691293dcb58bb97e4ba449225afba590190daa
delanggong <1765525967@qq.com>
2019-03-24 21:41:51 +0800

    Java-Code-验证移位运算是否循环

---
+++ Java/code/MoveOperator/src/com/demo/TestDemo.java
@@ -0,0 +1,35 @@
+package com.demo;
+
+public class TestDemo {
+	private static   final int num = -10;
+	private static  int forNum = num;
+	
+	/*主函数入口*/
+	public static void main(String[] args) {
+		System.out.println("-------- num的二进制  --------" );
+		System.out.println(Integer.toBinaryString(num));
+		
+		System.out.println("-------- 带符号右移32位  --------" );
+		System.out.println("十进制" + (num >> 31));
+		System.out.println("二进制" + Integer.toBinaryString(num >> 31));
+		
+		for(int i= 0 ; i< 32; ++i) {
+			forNum = forNum >> 1;
+			System.out.println("二进制" + Integer.toBinaryString(forNum));
+		}
+		
+		System.out.println("-------- for循环带符号右移32  --------" );
+		System.out.println("十进制" + forNum);
+		System.out.println("二进制" + Integer.toBinaryString(forNum));
+		
+		System.out.println("-------- 无符号右移1位  --------" );
+		System.out.println("十进制" + (num >>> 1));
+		System.out.println("二进制" + Integer.toBinaryString(num >>> 1));
+		
+		System.out.println("-------- 左移1位  --------" );
+		
+		System.out.println("十进制" + (num << 1));
+		System.out.println("二进制" + Integer.toBinaryString(num << 1));
+		
+	}
+}
