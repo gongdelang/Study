b684832086e3163c1a13288b3c03854bb7657164
delanggong <delanggong@qq.com>
2019-03-31 10:09:00 +0800

    使用Arrays.sort排序

--- Java/code/patternMatcher/src/Main.java
+++ Java/code/patternMatcher/src/Main.java
@@ -1,4 +1,6 @@
 
+
+import java.util.Arrays;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
@@ -8,11 +10,28 @@ public class Main {
     public static void main(String[] args)
     {
         /*提取字符串中数字，并且排序输出*/
-        String a="love23next234csdn3423javaeye";
-        String regEx="[^0-9]";
+        String numStr="ccytc67fc6r6cvui9bvu8vuvub0bib97gvyyb0h0bbp0h9g8c65d";
+
+        //提取数字，把其他字符去掉
+        String regEx="\\D";
         Pattern p = Pattern.compile(regEx);
-        Matcher m = p.matcher(a);
-        System.out.println( m.replaceAll("").trim());
+        Matcher m = p.matcher(numStr);
+        numStr=m.replaceAll("").trim();
+
+        /*排序数字字符串*/
+        char[] intArrNum = new char[numStr.length()];
+        int index = 0;
+        for(int i = 0; i< numStr.length(); ++i){
+            if(numStr.charAt(i)>=48 && numStr.charAt(i) <= 57){
+                intArrNum[index] = numStr.charAt(i);
+                index++;
+            }
+        }
+
+        Arrays.sort(intArrNum);//排序
+        for(char a : intArrNum){
+            System.out.print(a);
+        }
 
     }
 }
